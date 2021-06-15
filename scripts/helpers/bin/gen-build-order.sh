d2d695dc2e946ec186034d5ac2bafe8ea609205a
Christopher Loyd Nugent <cnugent@localhost.localdomain>
2021-06-15 12:54:52 -0500

    Began working on build order tree generator.

--- scripts/helpers/bin/gen-build-order.sh
+++ scripts/helpers/bin/gen-build-order.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/bash
+. scripts/helpers/common
+. ${HELPERS_DIR}/build-order-list-vars
+
+BUILD_TREE="";
+function construct_build_tree {
+
+}
+
+function prioritize_build_tree {
+
+}
 
 
+construct_build_tree
+prioritize_build_tree
+
+echo "${BUILD_TREE}"
