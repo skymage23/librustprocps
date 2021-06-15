42cad2974b3081a22c6243d7f1e5abc7af9678fd
Christopher Loyd Nugent <cnugent@localhost.localdomain>
2021-06-14 22:44:56 -0500

    Added ncurses.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ RUN apt-get update
 RUN DEBIAN_FRONTEND="noninteractive"; TZ="America/Chicago" apt-get -y install tzdata
 
 #Install build tools.
-RUN apt-get install -y build-essential libncurses6 libncurses-dev libtool libtool-bin automake autoconf gettext autopoint pkgconf
+RUN apt-get install -y build-essential libtool libtool-bin automake autoconf gettext autopoint pkgconf #libncusrse6 libncurses-dev
 
 #Install Rust:
 RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
--- scripts/build-depends.sh
+++ scripts/build-depends.sh
@@ -1,4 +1,31 @@
-#!/bin/sh
+#!/usr/bin/bash
+#[Chris Nugent]: Note to self:
+#  You just figured out how to get the dependencies to install into an alternate directory:
+#
+#  ./configure --prefix="<absolute_path_to_new_root>"
+#  Also, don't forget you need to script the creation and destruction
+#  of this suppossed new "root", henceforth called "build"
+#
+
+
+#Problem: exit_trap gets called twice.
+#EXIT_TRAP_CALLBACKS=""
+#EXIT_TRAP_CALLED=0
+#function exit_trap {
+#   #We may want to lock a file while we change this value:
+#   [ ${EXIT_TRAP_CALLED} -gt 0] && exit 0;
+#   #Obtain the lock. If lock succeeded, check value again.
+#   exec 1000>/tmp/build-depends-exit-trap.lock;
+#   flock
+   
+#   [ ${EXIT_TRAP_CALLED} -gt 0] && exit 0;
+#   export EXIT_TRAP_CALLED=1;
+#   for var in ${EXIT_TRAP_CALLBACKS}; do
+#       eval "${var}";
+#   done
+#}
+#trap exit_trap EXIT;
+#trap exit_trap SIGINT;
 
 
 #This function is intended to run in its own process:
@@ -22,6 +49,8 @@ function build_project () {
 
 #Process Pool:
 #Make mkfifo for "thread" pool
+#FFIO="build_depends_"
+
 
 #Main:
 DEPEND_DIR="dependencies"
@@ -31,6 +60,9 @@ BUILD_ROOT="${DEPEND_DIR}/build"
 if [ ! -d "${BUILD_ROOT}" ]; then
      mkdir ${BUILD_ROOT};
 fi
+
+#Build the "build order tree":
+
 
 #Iterate through the directories in DEPEND_DIR
 
