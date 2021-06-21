1c1f2cb432512c94337747906717ecf9df7b64a3
Christopher Loyd Nugent <cnugent@localhost.localdomain>
2021-06-20 21:34:19 -0500

    Finished the "test_the_tester" test runner

--- scripts/tests/test_the_tester/main.sh
+++ scripts/tests/test_the_tester/main.sh
@@ -2,6 +2,15 @@
 
 #Get list of directories:
 __counter=0;
-find . -maxdepth 1 -type d -print0 | while IFS= read -r -d '' filename; do
-   [ "${filename}" != '.' ] && echo "${filename}"
+
+find . -maxdepth 1 -type d -print0 | while IFS= read -r -d '' dirname; do
+    if [ "${dirname}" != '.' ]; then
+        (cd "${dirname}"; ./main.sh 2>&1 >/dev/null;)
+	echo -n "${filename}: "
+	if [ $? -lt 1 ]; then
+            echo "Test Succeeded."
+	else
+            echo "Test Failed."
+        fi
+    fi
 done
--- scripts/tests/test_the_tester/test-unit-test/main.sh
+++ scripts/tests/test_the_tester/test-unit-test/main.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+set -e
 . ./test_helpers/test_utils_header
 . ./test-stuff
 . ./test_helpers/test_utils_footer
